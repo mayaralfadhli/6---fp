dc510281d119608991afae6d115bce4ab9f069d5
mayaralfadhli <84667222+mayaralfadhli@users.noreply.github.com>
2022-07-08 18:29:06 +0300

    Initial commit

---
+++ 6 - fps/Assets/Bot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = Player.instance.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.hasPath)
+        {
+            if (!target != null)
+            {
+                agent.SetDestination(target.transform.position);
+            }
+        }
+    }
+}
